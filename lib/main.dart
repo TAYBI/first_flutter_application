25171406d7535470615ecf13de43078ff61cb708
Bilal <taybib4@gmail.com>
2023-05-13 13:24:02 +0100

    get familiare with flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,264 @@
+// ignore_for_file: avoid_print, prefer_const_literals_to_create_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
+// import 'dart:math' as math;
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: Text('Hommmmme Screen'),
+        ),
+        body: IconButton(
+          icon: Hero(
+            tag: 'https://picsum.photos/250?image=9',
+            child: Image.network('https://picsum.photos/250?image=9'),
+          ),
+          iconSize: 50,
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (_) => AboutScreen()),
+            );
+            // print('Pressed!');
+          },
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('About Screen'),
+        ),
+        body: Hero(
+          tag: 'https://picsum.photos/250?image=9',
+          child: Image.network('https://picsum.photos/250?image=9'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+///////////  Navigation
+// class AboutScreen extends StatelessWidget {
+//   const AboutScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.green,
+//           title: Text('About Screen'),
+//         ),
+//         body: ElevatedButton(
+//           child: Text('Home'),
+//           onPressed: () {
+//             Navigator.push(
+//               context,
+//               MaterialPageRoute(
+//                 builder: (_) => MyApp(),
+//               ),
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.blue,
+//           title: Text('Home Screen'),
+//         ),
+//         body: ElevatedButton(
+//           child: Text('About'),
+//           onPressed: () {
+//             Navigator.push(
+//               context,
+//               MaterialPageRoute(
+//                 builder: (_) => AboutScreen(),
+//               ),
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// ==============counter app======
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   // here put variables
+//   int count = 0;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.green,
+//           title: Text('Stateful Widget'),
+//         ),
+//         floatingActionButton: FloatingActionButton(
+//           child: Icon(Icons.add),
+//           onPressed: () {
+//             setState(() {
+//               count++;
+//             });
+//           },
+//         ),
+//         body: Center(
+//           child: Text(
+//             '$count',
+//             style: TextStyle(fontSize: 50),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+// ==============counter app======
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           backgroundColor: Colors.black38,
+//           title: const Text('im learning about flutter'),
+//         ),
+//         body: ListView.builder(
+//           itemBuilder: (_, index) {
+//             return Container(
+//               color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt())
+//                   .withOpacity(1.0),
+//               width: 500,
+//               height: 500,
+//             );
+//           },
+//         ),
+
+//         // ListView(
+//         //   scrollDirection: Axis.horizontal,
+//         //   children: [
+//         //     Container(
+//         //       color: Colors.blue,
+//         //       width: 500,
+//         //       height: 500,
+//         //     ),
+//         //     Container(
+//         //       color: Colors.orange,
+//         //       width: 500,
+//         //       height: 500,
+//         //     ),
+//         //     Container(
+//         //       color: Colors.green,
+//         //       width: 500,
+//         //       height: 500,
+//         //     ),
+//         //   ],
+//         // ),
+
+//         // Stack(
+//         //   children: [
+//         //     Container(
+//         //       color: Colors.orange,
+//         //       width: 100,
+//         //       height: 100,
+//         //       child: const Align(
+//         //         alignment: Alignment.center,
+//         //         child: Icon(Icons.sunny),
+//         //       ),
+//         //     ),
+//         //   ],
+//         // ),
+
+//         // floatingActionButton: FloatingActionButton(
+//         //   onPressed: () {
+//         //     print('pressed');
+//         //   },
+//         //   child: const Icon(Icons.add),
+//         // ),
+
+//         // bottomNavigationBar: BottomNavigationBar(
+//         //   items: [
+//         //     BottomNavigationBarItem(
+//         //       icon: Icon(Icons.home),
+//         //       label: 'Home',
+//         //     ),
+//         //     BottomNavigationBarItem(
+//         //       icon: Icon(Icons.school),
+//         //       label: 'School',
+//         //     ),
+//         //     BottomNavigationBarItem(
+//         //       icon: Icon(Icons.business),
+//         //       label: 'Business',
+//         //     ),
+//         //   ],
+//         // ),
+
+//         // drawer: Drawer(
+//         //   child: Text('Drawer................!!'),
+//         // ),
+//         // const Row(
+//         //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//         //     crossAxisAlignment: CrossAxisAlignment.end,
+//         //     children: [
+//         //       Expanded(
+//         //         flex: 4,
+//         //         child: Icon(Icons.abc_outlined),
+//         //       ),
+//         //       Icon(Icons.person),
+//         //       Icon(Icons.leaderboard),
+//         //       Icon(Icons.back_hand)
+//         //     ]),
+
+//         //  Center(
+//         //   child: Container(
+//         //     child: const Text("Here I am"),
+//         //     padding: const EdgeInsets.all(50),
+//         //     color: Colors.yellowAccent,
+//         //   ),
+//         // ),
+//       ),
+//     );
+//   }
+// }
